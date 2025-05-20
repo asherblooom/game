9660251b9758364908cca53be2ecb0e93c91036e
Asher Bloom <si23870@bristol.ac.uk>
2025-05-20 18:51:25 +0100

    create first window

--- Makefile
+++ Makefile
@@ -1,5 +1,12 @@
-linux: src/main.cpp
-	gcc -Wall -Wextra -Iinclude/ -lglfw -lGL -lX11 -lpthread -lXrandr -lXi -ldl src/main.cpp -o main.o
+linux: main glad
+	g++ -lglfw -lGL -lX11 -lpthread -lXrandr -lXi -ldl -o game.o main.o glad.o
 
-windows: src/main.cpp
+windows: main glad
 	# add something here?
+
+main: src/main.cpp 
+	g++ -Wall -Wextra -Iinclude/ src/main.cpp -o main.o -c
+
+glad: src/glad.c
+	g++ -Iinclude/ src/glad.c -o glad.o -c
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,52 @@
 #include <glad/glad.h>
 
 #include <GLFW/glfw3.h>
-#include <stdio.h>
+#include <iostream>
 
-int main() {
-  printf("hiii");
-  return 0;
+const unsigned int SCR_WIDTH = 1920;
+const unsigned int SCR_HEIGHT = 1080;
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+int main()
+{
+	// Initialise glfw and set options
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// create window
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Game?", NULL, NULL);
+	if(window == NULL)
+	{
+		std::cout << "Failed to create GLFW window\n";
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	// initialise GLAD - manages function pointers for OpenGL
+	if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD\n";
+		return -1;
+	}
+
+	glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	while(!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
 }
