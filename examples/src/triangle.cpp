fe2ff026b15b5459701480bd5d36bf151deb315c
Asher Bloom <si23870@bristol.ac.uk>
2025-05-21 16:38:32 +0100

    add error handling to shader compilation

--- examples/src/triangle.cpp
+++ examples/src/triangle.cpp
@@ -5,13 +5,12 @@
 
 const unsigned int SCR_WIDTH = 1920;
 const unsigned int SCR_HEIGHT = 1080;
-const GLfloat background_colour[] = {0.2f, 0.3f, 0.3f, 1.0f};
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void process_input(GLFWwindow* window);
 GLuint compile_shaders(void);
 
-int main()
+int main(void)
 {
 	// Initialise glfw and set options
 	glfwInit();
@@ -46,11 +45,14 @@ int main()
 	glCreateVertexArrays(1, &vertex_array_object);
 	glBindVertexArray(vertex_array_object);
 
+	//Initialise variables
+	const GLfloat background_color[] = {0.2f, 0.3f, 0.3f, 1.0f};
+
 	// Event loop
 	while(!glfwWindowShouldClose(window))
 	{
 		process_input(window);
-		glClearBufferfv(GL_COLOR, 0, background_colour);
+		glClearBufferfv(GL_COLOR, 0, background_color);
 
 		glUseProgram(rendering_program);
 
@@ -87,6 +89,9 @@ GLuint compile_shaders(void)
 	GLuint fragment_shader;
 	GLuint program;
 
+	int success;
+	char info_log[512];
+
 	static const GLchar* vertex_shader_source[] = {
 		"#version 450 core                                  \n"
 		"                                                   \n"
@@ -116,16 +121,40 @@ GLuint compile_shaders(void)
 	glShaderSource(vertex_shader, 1, vertex_shader_source, NULL);
 	glCompileShader(vertex_shader);
 
+	// check for compile-time errors
+	glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success);
+	if(!success)
+	{
+		glGetShaderInfoLog(vertex_shader, 512, NULL, info_log);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << info_log << "\n";
+	}
+
 	// Create and compile fragment shader
 	fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(fragment_shader, 1, fragment_shader_source, NULL);
 	glCompileShader(fragment_shader);
+
+	// check for compile-time errors
+	glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &success);
+	if(!success)
+	{
+		glGetShaderInfoLog(fragment_shader, 512, NULL, info_log);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << info_log << "\n";
+	}
 
 	// Create program, attach shaders to it, and link it
 	program = glCreateProgram();
 	glAttachShader(program, vertex_shader);
 	glAttachShader(program, fragment_shader);
 	glLinkProgram(program);
+
+	// check for linking errors
+	glGetProgramiv(program, GL_LINK_STATUS, &success);
+	if(!success)
+	{
+		glGetProgramInfoLog(program, 512, NULL, info_log);
+		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << info_log << "\n";
+	}
 
 	// Delete the shaders as the program has them now
 	glDeleteShader(vertex_shader);
